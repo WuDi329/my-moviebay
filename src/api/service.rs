63df88fbbe6cc5702a9473118e2601e478bab688
wudi <wuuudiii@qq.com>
2023-03-03 16:21:16 +0800

    sync

---
+++ src/api/service.rs
@@ -0,0 +1,110 @@
+use hyper::service::Service;
+use hyper::{Body, Request, Response};
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use super::{
+    handler,
+    router::{Handler, Route, Router},
+};
+use crate::config::SharedCfg;
+use crate::sqlite::SharedDb;
+use serde::Serialize;
+
+type FuturePin<T> = Pin<Box<dyn Future<Output = T> + Send + Sync + 'static>>;
+
+#[derive(Debug, Serialize)]
+struct Movie {
+    id: i32,
+    title: String,
+    release_year: i32,
+    file_path: String,
+    poster_path: String,
+    backdrop_path: String,
+}
+
+pub struct ApiService {
+    config: SharedCfg,
+    db: SharedDb,
+    router: Router,
+}
+
+impl ApiService {
+    // new函数为ApiService添加了相应的路径
+    fn new(db: SharedDb, config: SharedCfg) -> ApiService {
+        let mut router = Router::new();
+        router.add(Route::get(r"/movies/(\d+)").name("get_movie"));
+        router.add(Route::get("/movies/").name("get_movies"));
+        router.add(Route::get(r"/stream/(\d+)").name("get_stream"));
+        ApiService { config, db, router }
+    }
+}
+
+impl Service<Request<Body>> for ApiService {
+    type Response = Response<Body>;
+    type Error = hyper::Error;
+    type Future = FuturePin<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        if let Some(route) = self.router.is_match(req.uri().path()) {
+            let res: Handler = match route.name.as_ref() {
+                "get_movies" => Box::pin(handler::get_movies(self.db.clone())),
+                "get_movie" => {
+                    let id = route.params[0].parse().unwrap();
+                    Box::pin(handler::get_movie(self.db.clone(), id))
+                }
+                // 如果是get_stream 就匹配到handler中的service方法
+                "get_stream" => {
+                    let id = route.params[0].parse().unwrap();
+                    Box::pin(handler::get_stream(
+                        self.db.clone(),
+                        self.config.clone(),
+                        id,
+                    ))
+                }
+                _ => unimplemented!(),
+            };
+            return res;
+        }
+
+        Box::pin(async { Ok(Response::builder().body(Body::from("Not Found")).unwrap()) })
+    }
+}
+
+pub struct MakeApiSvc {
+    config: SharedCfg,
+    db: SharedDb,
+}
+
+impl MakeApiSvc {
+    pub fn new(config: SharedCfg, db: SharedDb) -> MakeApiSvc {
+        MakeApiSvc { config, db }
+    }
+}
+
+// 为MakeApiSvc实现了Service接口
+impl<T> Service<T> for MakeApiSvc {
+    type Response = ApiService;
+    type Error = hyper::Error;
+    type Future = FuturePin<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+    fn call(&mut self, _: T) -> Self::Future {
+        // 获取MakeApiSvc的config和db，这里直接使用clone方法
+        let config = self.config.clone();
+        let db = self.db.clone();
+
+        // routes
+
+        //  在这里通过一个service初始化了另外一个 service ，也就是ApiService
+        let fut = async move { Ok(ApiService::new(db, config)) };
+        Box::pin(fut)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,66 @@
+mod api;
+mod config;
+mod context;
+mod ffmpeg;
+mod model;
+mod scan;
+mod sqlite;
+mod tmdb;
+
+use crate::api::MakeApiSvc;
+use crate::config::Config;
+use crate::context::Context;
+use crate::model::{MovieTable, Table};
+use crate::scan::Scanner;
+use crate::sqlite::{params, Connection};
+use hyper::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ctx = Context::from_config(Config::new());
+    //config 的sqlite和config 来源于ctx
+    let sqlite = ctx.db();
+    let config = ctx.cfg();
+
+// 这里是tmdb的相关设置，和ffmpeg的相关代码没有关系
+    // let tmdb_cfg = tmdb::fetch_configuration(config.tmdb.clone())
+    //     .await
+    //     .unwrap();
+    // let movies = tmdb::search_movie(config.tmdb.clone(), "Collateral", 2004)
+    //     .await
+    //     .unwrap();
+
+    // // println!("{:?}", tmdb_cfg);
+    // // println!("{:?}", movies);
+    
+    // let table = MovieTable::new(sqlite.clone());
+    // table.create_table().await?;
+
+    // let mut scanner = Scanner::new(config.clone());
+    // let movies = scanner.run()?;
+
+    // for movie in movies {
+    //     println!("{:?}", movie);
+
+    //     let m = movie.clone();
+    //     sqlite
+    //         .spawn(Box::new(move |conn: &Connection| {
+    //             conn.execute(
+    //                 "INSERT INTO movies (tmdb_id, title, overview, release_year, file_path, poster_path,  backdrop_path) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
+    //                 params![0, m.title, "", m.release_year, m.path.to_str(), m.poster_path, m.backdrop_path],
+    //             )
+    //         }))
+    //         .await?;
+    // }
+
+    // sqlite.save().await?;
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    // 这里初始化了MakeApiSvc 的new方法
+    let server = Server::bind(&addr).serve(MakeApiSvc::new(config, sqlite));
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+    Ok(())
+}
